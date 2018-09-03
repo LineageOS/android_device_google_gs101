ea90d8eb65c0ab2bf7a958acfb63fb07e97befbf
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- lineage_common.mk
+++ lineage_common.mk
@@ -4,4 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-lineage
+
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
