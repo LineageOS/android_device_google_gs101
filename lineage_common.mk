582e218e9f99778ae0df41093fec4bcbb04868a4
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- lineage_common.mk
+++ lineage_common.mk
@@ -12,6 +12,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
