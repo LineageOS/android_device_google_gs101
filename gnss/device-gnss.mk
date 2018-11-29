cbbd52b0e71d6a4151329dd40f071af71d26ea3f
Ethan Chen <intervigil@gmail.com>
2018-11-28 20:41:20 -0800

    Disable userdebug diagnostics
    * We can't make use of most of the debug features on userdebug builds.
    
    Change-Id: I31df2090b31243b742c4e82dc08cd8491dada54f

--- gnss/device-gnss.mk
+++ gnss/device-gnss.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES += \
 	device/google/gs101/gnss/47765/config/gps.cer:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.cer \
 	device/google/gs101/gnss/47765/firmware/SensorHub.patch:$(TARGET_COPY_OUT_VENDOR)/firmware/SensorHub.patch
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 	PRODUCT_COPY_FILES += \
 		device/google/gs101/gnss/47765/config/lhd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/lhd.conf \
 		device/google/gs101/gnss/47765/config/scd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf
@@ -14,19 +14,19 @@ else
 endif
 
 ifneq (,$(filter raven, $(subst _, ,$(TARGET_PRODUCT))))
-	ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+	ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 		PRODUCT_COPY_FILES += device/google/gs101/gnss/47765/config/gps.xml.raven:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 	else
 		PRODUCT_COPY_FILES += device/google/gs101/gnss/47765/config/gps_user.xml.raven:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 	endif
 else ifneq (,$(filter oriole, $(subst _, ,$(TARGET_PRODUCT))))
-	ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+	ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 		PRODUCT_COPY_FILES += device/google/gs101/gnss/47765/config/gps.xml.oriole:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 	else
 		PRODUCT_COPY_FILES += device/google/gs101/gnss/47765/config/gps_user.xml.oriole:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 	endif
 else ifneq (,$(filter slider whitefin, $(subst _, ,$(TARGET_PRODUCT))))
-	ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+	ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 		PRODUCT_COPY_FILES += device/google/gs101/gnss/47765/config/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 	else
 		PRODUCT_COPY_FILES += device/google/gs101/gnss/47765/config/gps_user.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
@@ -45,5 +45,7 @@ PRODUCT_PACKAGES += \
 	scd \
 	android.hardware.gnss@2.1-service-brcm
 
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES_DEBUG += \
 	init.gps_log.rc
+endif
