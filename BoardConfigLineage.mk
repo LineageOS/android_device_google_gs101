c38241474b8e7dbbc8d577ecb9a31c575f4e2036
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 11:13:09 -0700

    gs101: Build a disabled vbmeta image
    Squashed with:
    Author: Nolen Johnson <johnsonnolen@gmail.com>
    Date:   Sun May 9 16:04:34 2021 -0400
    
        Simplify AVB flag logic
    
        * AVB's `--flags 3` argument builds a disabled VBMeta with
          disabled hashtree, so no need to set both flags individually.
    
        Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838
    
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -28,6 +28,9 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 AB_OTA_PARTITIONS += \
     vbmeta_vendor
 
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Reserve space for gapps install
 ifneq ($(WITH_GMS),true)
 BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
