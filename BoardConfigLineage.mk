3e57a774facf125b0e89becbb8649a4942976749
Alessandro Astone <ales.astone@gmail.com>
2021-01-30 00:55:01 +0100

    Build pixel touch HAL
    Change-Id: I45cff81e1159613094440dd7b2b73b53519c9de6

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -7,6 +7,7 @@
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Manifests
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += device/google/gs101/manifest_radio.xml
 
 # Partitions
