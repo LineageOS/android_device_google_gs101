548f6b81c4b6a8b713b41fd423fedb9c35a5f95c
Ilya Matyukhin <ilyamaty@google.com>
2021-03-19 19:58:07 +0000

    Revert "Include the Soong namespace of the UDFPS HAL"
    Revert submission 13892440-integrate-goodix-aidl
    
    Reason for revert: b/183225087
    Reverted Changes:
    I928a7f4d6:Include the Soong namespace of the UDFPS HAL
    I7acecd294:Transition to Android.bp
    I91bdbceef:Merge the latest goodix AIDL drop
    
    Change-Id: I8799dbcd629e74bd25e8b7d0026af432afc0a5b0

--- fingerprint/udfps.mk
+++ fingerprint/udfps.mk
@@ -17,8 +17,5 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# Include the Goodix AIDL HAL namespace.
-PRODUCT_SOONG_NAMESPACES += vendor/goodix/udfps/g6_trusty
-
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.goodix
