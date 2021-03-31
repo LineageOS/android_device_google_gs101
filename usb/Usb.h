88313cf20b653789160189eef263014b7036e264
Ricky Niu <rickyniu@google.com>
2021-03-31 15:54:30 +0800

    gs101: usb: remove flag "OLDER_GADGET"
    The flag "OLDER_GADGET" is no need.
    Using "11110000.dwc3" be the udc name.
    
    Test: Build Pass and boot to home
    Bug: 177879761
    Signed-off-by: Ricky Niu <rickyniu@google.com>
    Change-Id: I43c9e2815a6384c4dffdc47312fa0f372b34d9e9

--- usb/Usb.h
+++ usb/Usb.h
@@ -66,13 +66,8 @@ enum class HALVersion{
     V1_3
 };
 
-#ifdef OLDER_GADGET
 constexpr char kGadgetName[] = "11110000.dwc3";
 #define UDC_PATH "/sys/class/udc/11110000.dwc3/"
-#else
-constexpr char kGadgetName[] = "11110000.usb";
-#define UDC_PATH "/sys/class/udc/11110000.usb/"
-#endif
 
 #define ID_PATH UDC_PATH "device/id"
 #define VBUS_PATH UDC_PATH "device/b_sess"
