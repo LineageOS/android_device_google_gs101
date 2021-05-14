24b12e293540d6bc7f4beeb3c7aefedd0a6f8bee
Daniel Mentz <danielmentz@google.com>
2021-05-13 17:26:39 -0700

    Add a kernel module blocklist file
    Kernel modules on the blocklist are not automatically loaded during
    second stage init. Modules are often put on the blocklist if we want
    them to get loaded only under certain circumstances.
    
    Bug: 190652328
    Change-Id: Ib67463555f498f4dc85d5d175e0bf9e4150771dd

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -379,6 +379,8 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 # Vendor ramdisk image for kernel development
 BOARD_BUILD_VENDOR_RAMDISK_IMAGE := true
 
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := device/google/gs101/vendor_dlkm.blocklist
+
 KERNEL_MODULE_DIR := $(TARGET_KERNEL_DIR)
 KERNEL_MODULES := $(wildcard $(KERNEL_MODULE_DIR)/*.ko)
 
