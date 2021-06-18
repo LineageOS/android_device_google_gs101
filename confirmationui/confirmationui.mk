56978d99612712273419c599a2897df0770a4509
Adam Shih <adamshih@google.com>
2021-06-18 09:30:50 +0800

    organize confirmationui settings
    Bug: 190331547
    Bug: 190331370
    Test: build ROM and make sure file and sepolicy is still there
    Change-Id: Ifc2bcf1616d3ee2df3943f0f1b3523ce485b689c

---
+++ confirmationui/confirmationui.mk
@@ -0,0 +1,9 @@
+# Trusty ConfirmationUI HAL
+PRODUCT_PACKAGES += \
+	android.hardware.confirmationui@1.0-service.trusty.vendor
+
+# Trusty Secure DPU Daemon
+PRODUCT_PACKAGES += \
+	securedpud.slider
+
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101-sepolicy/confirmationui/
