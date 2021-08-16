957e03aca25139ff6fff4e14dd7d760dcdd93531
George Chang <georgekgchang@google.com>
2021-08-16 23:51:10 +0800

    Remove NFC/SE settings from common gs101
    Bug: 196825539
    Test: build pass
    Merged-In: I1833e3206403f1fce30377daaf942e617ed499b7
    Change-Id: I1833e3206403f1fce30377daaf942e617ed499b7

--- device.mk
+++ device.mk
@@ -933,25 +933,6 @@ PRODUCT_PACKAGES_DEBUG += \
 	diag-vibrator-drv2624 \
 	$(NULL)
 
-# NFC
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
-	frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-	frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-	frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
-	frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
-	frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml
-
-PRODUCT_PACKAGES += \
-	NfcNci \
-	Tag \
-	android.hardware.nfc@1.2-service.st
-
-# SecureElement
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
-	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
-
 PRODUCT_PACKAGES += \
 	android.hardware.health@2.1-impl-gs101 \
 	android.hardware.health@2.1-service
