7544f82513bb678096bb2d6c76f6cee952cca3b2
Danny Lin <danny@kdrag0n.dev>
2021-11-13 21:20:48 -0800

    overlay: Settings: Override boosted color mode
    The Pixel 6 series of devices (raven/oriole) uses vendor color mode 256
    for Boosted, rather than the AOSP color mode 1.
    
    Change-Id: I046b24f86ecc8b13ee4ac7485d143843c21bd9d5

--- lineage_common.mk
+++ lineage_common.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-lineage
+
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
