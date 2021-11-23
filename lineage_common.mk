fa5056578fe8d46a95216f560576f5291d336aeb
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-23 15:05:41 -0500

    Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- lineage_common.mk
+++ lineage_common.mk
@@ -7,6 +7,11 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-lineage
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
