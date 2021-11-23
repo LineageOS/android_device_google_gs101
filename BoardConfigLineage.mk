65aa0e8f89c43a2c9c6c21e58d726e7019b99f5e
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-23 22:32:25 +0530

    gs101: Build vendor_dlkm
    Change-Id: Ic2a0e4cdfdcc448a73bef41e0d2140e9d0aefcf0

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor_dlkm
+
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
