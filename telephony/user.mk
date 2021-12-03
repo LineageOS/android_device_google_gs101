4c7647c5e52aec6c693ae06cf16f36d5f7bc7434
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 02:22:58 +0530

    gs101: Allow building vendor image
    * We don't have access to these files, but we still want to
      build vendor images without errors
    
    Change-Id: I4c09f7134b218b5fc10d180a6ef05015cad54f5d

--- telephony/user.mk
+++ telephony/user.mk
@@ -1,2 +1,4 @@
+ifneq ($(wildcard vendor/samsung_slsi/telephony/),)
 PRODUCT_COPY_FILES += vendor/samsung_slsi/telephony/common/device/samsung/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh
+endif
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101/sepolicy/telephony/user/
