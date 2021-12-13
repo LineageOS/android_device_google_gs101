db2723b1b57ddd6250b8007107a65b86ac450f2f
matthuang <matthuang@google.com>
2021-12-13 12:43:00 +0800

    Don't copy unsupported sensor xml.
    Use findstring to filter the factory target.
    
    Bug: 209918222
    Test: Verify the unsupported  xml files are not in the device.
    Change-Id: I1383b18825ad06e88dedc9b4950bc5e5f676ecb8

--- device.mk
+++ device.mk
@@ -346,7 +346,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # Sensors
-ifneq (,$(filter citron tangor, $(TARGET_PRODUCT)))
+ifneq (,$(findstring tangor, $(TARGET_PRODUCT)))
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
         frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
