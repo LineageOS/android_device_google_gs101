eed4ae2a53a9de4c78186ce2236082cb74e15d61
Terry Huang <terrycrhuang@google.com>
2022-01-06 07:43:17 +0000

    Revert "Revert "[s5123][device] Change vendor telephony code path""
    This reverts commit 479f361d7734b0f77d084892a2f0f7299e23488a.
    
    Reason for revert: Restore 16405845-t-codebase-seperate
    
    Change-Id: Id7f5e8b7e4470416da0002768c3960e554c0cf6c

--- telephony/user.mk
+++ telephony/user.mk
@@ -1,2 +1,2 @@
-PRODUCT_COPY_FILES += vendor/samsung_slsi/telephony/common/device/samsung/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh
+PRODUCT_COPY_FILES += vendor/samsung_slsi/telephony/$(BOARD_USES_SHARED_VENDOR_TELEPHONY)/common/device/samsung/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101-sepolicy/telephony/user/
