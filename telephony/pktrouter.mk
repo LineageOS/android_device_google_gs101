18daae9c560b029c9c8d28d2bee2c735b11743a0
Scott Warner <Tortel1210@gmail.com>
2022-01-06 15:37:05 -0500

    Update sepolicy path
    Change-Id: I4bcee50514c02294137ea5ac6e64c64d69308bd4

--- telephony/pktrouter.mk
+++ telephony/pktrouter.mk
@@ -1,3 +1,3 @@
 PRODUCT_PACKAGES += wfc-pkt-router
 PRODUCT_PROPERTY_OVERRIDES += vendor.pktrouter=1
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101-sepolicy/telephony/pktrouter
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101/sepolicy/telephony/pktrouter
