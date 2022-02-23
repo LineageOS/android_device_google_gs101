0fdfb92e77522198e53567e5cf9a7ad589a0e2ae
Cole Faust <colefaust@google.com>
2022-02-22 17:08:42 -0800

    Use soong_config_set instead of raw variables
    soong_config_set works better with the conversion
    to Starlark.
    
    Bug: 220940864
    Test: Presubmits
    Change-Id: Id71c1167d43db21e42e68264a2870153f850c9f8

--- fingerprint/extension/fingerprint.extension.mk
+++ fingerprint/extension/fingerprint.extension.mk
@@ -1,7 +1,6 @@
 # Fingerprint extension feature
 ifneq (,$(filter userdebug, $(TARGET_BUILD_VARIANT)))
-SOONG_CONFIG_fp_hal_feature += report_bug_support
-SOONG_CONFIG_fp_hal_feature_report_bug_support := true
+$(call soong_config_set,fp_hal_feature,report_bug_support,true)
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/fingerprint-extension/vendor/
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/fingerprint-extension/system_ext/public/
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/fingerprint-extension/system_ext/private/
