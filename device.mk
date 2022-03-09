61bda99af9d4558925fb9b32615263dadca1ac96
Ben Murdoch <benm@google.com>
2022-03-09 11:01:54 +0000

    handheld_core_hardware.xml should be included at device specific layer.
    Bug: 223543820
    Test: Manual, verify build artifacts
    Change-Id: I22d8e3d599a4453fddc91541e622931ccd20f961

--- device.mk
+++ device.mk
@@ -918,8 +918,7 @@ $(call inherit-product-if-exists, vendor/google/camera/devices/whi/device-vendor
 
 PRODUCT_COPY_FILES += \
 	device/google/gs101/default-permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions.xml \
-	device/google/gs101/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-	frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
+	device/google/gs101/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # modem_svc_sit daemon
 PRODUCT_PACKAGES += modem_svc_sit
