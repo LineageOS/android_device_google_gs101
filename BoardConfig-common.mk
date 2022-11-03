0a2cb7504b783518ee2a2059520c8c509eb089b1
Michael Bestas <mkbestas@lineageos.org>
2022-11-03 18:34:02 +0200

    Unset BOARD_USES_EXYNOS_SENSORS_DUMMY
    * Matches stock vendor
    
    Change-Id: Ia8cec2c3161928863bccfc97a9222b490379bf26

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -274,9 +274,6 @@ BOARD_SECCOMP_POLICY = device/google/gs101/seccomp_policy
 #CURL
 BOARD_USES_CURL := true
 
-# Sensor HAL
-BOARD_USES_EXYNOS_SENSORS_DUMMY := true
-
 # VISION
 # Exynos vision framework (EVF)
 #TARGET_USES_EVF := true
