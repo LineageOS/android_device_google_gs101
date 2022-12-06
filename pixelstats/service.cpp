851f42f316c05d3458127de0eaaff314090c0871
Michael Bestas <mkbestas@lineageos.org>
2022-12-06 16:29:42 +0200

    [SQUASH] Merge tag 'android-13.0.0_r16' into staging/lineage-20_merge-android-13.0.0_r16
    Change-Id: If2dff59634ee7c6cb2d7b866a390d2ef561c652b

--- pixelstats/service.cpp
+++ pixelstats/service.cpp
@@ -25,6 +25,7 @@
 using android::hardware::google::pixel::SysfsCollector;
 using android::hardware::google::pixel::UeventListener;
 
+#define BLOCK_STATS_LENGTH 17
 #define UFSHC_PATH(filename) "/dev/sys/block/bootdevice/" #filename
 #define UFS_ERR_PATH(err_type) UFSHC_PATH(err_stats/) #err_type
 const struct SysfsCollector::SysfsPaths sysfs_paths = {
@@ -52,7 +53,8 @@ const struct SysfsCollector::SysfsPaths sysfs_paths = {
             UFS_ERR_PATH(dme_err_count),
             UFS_ERR_PATH(fatal_err_count),
             UFS_ERR_PATH(auto_hibern8_err_count)
-        }
+        },
+        .AmsRatePath = "/sys/devices/platform/audiometrics/ams_rate_read_once",
 };
 
 const struct UeventListener::UeventPaths ueventPaths = {
