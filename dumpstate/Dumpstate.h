989d016d987a4a9dd9efd6978cd7d7896a1599d5
Adam Shih <adamshih@google.com>
2022-12-08 11:28:24 +0800

    move dump cpu to gs-common
    Bug: 240530709
    Test: adb bugreport
    Change-Id: I073a4c55053b8844b2702c9cd5d7a9e84840b66d

--- dumpstate/Dumpstate.h
+++ dumpstate/Dumpstate.h
@@ -48,7 +48,6 @@ class Dumpstate : public BnDumpstateDevice {
     // Text sections that can be dumped individually on the command line in
     // addition to being included in full dumps
     void dumpPowerSection(int fd);
-    void dumpCpuSection(int fd);
     void dumpDevfreqSection(int fd);
     void dumpMemorySection(int fd);
     void dumpDisplaySection(int fd);
