5e5af5c774fac7f1c5ad7b383979d7f1159190da
Jooyung Han <jooyung@google.com>
2023-05-19 15:31:43 +0900

    Install Fingerprint HAL APEX
    Fingerprint HAL APEX packages the HAL binary along with its Trusty APP
    (g7.app). Permission file is also bundled in the apex, so we don't need
    to copy it separately.
    
    For now, the FP APEX is only used when its Trusty App is specified.
    
    Bug: 279674662
    Test: fingerprint works.
    Change-Id: Ia8483cc699cadf28eb258087dcda3e89ca8127ca

--- fingerprint/udfps_shipping.mk
+++ fingerprint/udfps_shipping.mk
@@ -14,8 +14,12 @@
 # limitations under the License.
 #
 
+ifneq (,$(call soong_config_get,goodix,fingerprint_ta))
+PRODUCT_PACKAGES += com.google.android.hardware.biometrics.fingerprint
+else
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.goodix
+endif
