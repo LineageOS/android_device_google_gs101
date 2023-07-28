4112c0f397fcd19bc9b8866a540c0f583b767375
Ram Chandrasekar <rchandrasekar@google.com>
2023-07-28 15:53:15 -0700

    gs101: update the thermal dump script path
    Update the thermal dump script path.
    
    Bug: 289146743
    Test: Compilation test.
    
    Change-Id: I21f138dd28c6e31496003eabe908ab478a509f13
    Signed-off-by: Ram Chandrasekar <rchandrasekar@google.com>

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ include device/google/gs-common/soc/soc.mk
 include device/google/gs-common/soc/freq.mk
 include device/google/gs-common/modem/modem.mk
 include device/google/gs-common/aoc/aoc.mk
-include device/google/gs-common/thermal/thermal.mk
+include device/google/gs-common/thermal/dump/thermal.mk
 include device/google/gs-common/pixel_metrics/pixel_metrics.mk
 include device/google/gs-common/performance/perf.mk
 include device/google/gs-common/display/dump.mk
