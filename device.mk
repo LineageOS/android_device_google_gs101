ca6b06a48f87cbf6f1e03171011da814fbf70a14
joonhunshin <joonhunshin@google.com>
2023-08-11 01:24:59 +0000

    Removing TestRcsApp userdebug application from the system image.
    RCS has a low priority and will not be used in the future, remove it from the system image.
    
    Bug: 295403727
    Test: Make sure the TestRcsApp is removed from the system image.
    
    Change-Id: I5bff88e66f4ecf43236dab8c3dbbd836cec9f91b

--- device.mk
+++ device.mk
@@ -889,10 +889,8 @@ PRODUCT_PACKAGES += ShannonIms
 
 $(call inherit-product-if-exists, vendor/samsung_slsi/telephony/$(BOARD_USES_SHARED_VENDOR_TELEPHONY)/shannon-iwlan/device-vendor.mk)
 
-#RCS Test Messaging App
 PRODUCT_PACKAGES_DEBUG += \
-	preinstalled-packages-product-gs101-device-debug.xml \
-	TestRcsApp
+	preinstalled-packages-product-gs101-device-debug.xml
 
 PRODUCT_PACKAGES += ShannonRcs
 endif
