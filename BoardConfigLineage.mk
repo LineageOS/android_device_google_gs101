1824a0993237a485d6d4328641c89d30da818216
Michael Bestas <mkbestas@lineageos.org>
2023-11-22 15:51:28 +0200

    gs101: Inherit common partition reserved sizes
    Change-Id: I444ce2fcd37cdfa58d83a7e872dc547fb05a2a89

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -31,5 +31,8 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 AB_OTA_PARTITIONS += \
     vbmeta_vendor
 
+# Reserve space for gapps install
+-include vendor/lineage/config/BoardConfigReservedSize.mk
+
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
