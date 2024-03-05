7b565852240477775fbc408b89189ad259716a53
Hongyang Jiao <jiaohy@google.com>
2024-03-05 23:24:05 +0000

    Add betterbug.mk to gs101
    Test: manual test
    Bug: 322543833
    Change-Id: Iccf4ceabd1c994b804de9e74b50fa8a0ca936af6

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ include device/google/gs-common/sota_app/factoryota.mk
 include device/google/gs-common/misc_writer/misc_writer.mk
 include device/google/gs-common/gyotaku_app/gyotaku.mk
 include device/google/gs-common/bootctrl/bootctrl_aidl.mk
+include device/google/gs-common/betterbug/betterbug.mk
 ifneq ($(filter oriole raven bluejay, $(TARGET_PRODUCT)),)
   include device/google/gs-common/bcmbt/dump/dumplog.mk
 endif
