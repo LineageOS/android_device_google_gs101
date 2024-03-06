4bb66fedc8a6f138be1c6ba4bc4ed1cd6b044884
Shashwat Razdan <srazdan@google.com>
2024-03-05 23:45:49 -0800

    Adding android.software.contextualsearch to gs-101 devices.
    Bug: 326143814
    Test: Built and tested on device
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4ff9169f2f92191802e70fadb83810d03d2e9952)
    Merged-In: Id45a24ec3361caade132836e5e849940efa6ac2c
    Change-Id: Id45a24ec3361caade132836e5e849940efa6ac2c
    CP feature CLs to 24d1-dev

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.vulkan.version-1_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
 	frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
 	frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
+	frameworks/native/data/etc/android.software.contextualsearch.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.contextualsearch.xml \
 	frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
 	frameworks/native/data/etc/android.software.opengles.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
