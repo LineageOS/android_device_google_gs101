395c89ec0559a9f9b8172f3005f2ee4854f401b0
Chungro Lee <chungro@google.com>
2024-03-12 17:34:01 +0000

    pixelstats: move learning/model load path to sysfs
    Reporting learning(relaxation) and model loading once a day
     * learning and model loading path moved from uevent to
       sysfs collector.
    
    Bug: 329124192
    Signed-off-by: Chungro Lee <chungro@google.com>
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e0d7a249370a93828b128cbffbab7ca9237cad90)
    Merged-In: I91039ce3a8ee4630a76113ef7ae6be3bc6908f64
    Change-Id: I91039ce3a8ee4630a76113ef7ae6be3bc6908f64

--- pixelstats/service.cpp
+++ pixelstats/service.cpp
@@ -73,18 +73,18 @@ const struct SysfsCollector::SysfsPaths sysfs_paths = {
             "/sys/class/power_supply/maxfg/gmsr",
             "/sys/class/power_supply/maxfg_base/gmsr",
         },
+        .FGLearningPath = {
+            "/sys/class/power_supply/maxfg/fg_learning_events"
+        },
+        .FGModelLoadingPath = {
+            "/sys/class/power_supply/maxfg/m5_model_state"
+        }
 };
 
 const struct UeventListener::UeventPaths ueventPaths = {
         .AudioUevent = "/devices/virtual/amcs/amcs",
         .TypeCPartnerUevent = "PRODUCT_TYPE=",
-        .FGLearningPath = {
-            "/sys/class/power_supply/maxfg/fg_learning_events"
-        },
-        .FwUpdatePath = "",
-        .FGModelLoadingPath = {
-            "/sys/class/power_supply/maxfg/m5_model_state"
-        }
+        .FwUpdatePath = ""
 };
 
 int main() {
