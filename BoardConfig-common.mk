d935f83aa4c8ab6e4e8eebaf2b6d23bf6ec5bf47
Bill Yang <yangbill@google.com>
2024-10-22 10:36:29 +0000

    Set BOARD_LIBACRYL_G2D_HDR_PLUGIN for soong
    Set the full module path of libacryl_hdr_plugin for libacryl conversion
    to soong. It will take effect after aosp/3286932 and aosp/3287183
    merged.
    
    Bug: 370414334
    Test: lunch aosp_riole-trunk_staging-userdebug ; m libacryl_hdr_plugin libacryl
    Change-Id: I67339b5420d523d9a61e0457d77a1412ae6b3582

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -247,6 +247,7 @@ BOARD_USES_EXYNOS_AFBC_FEATURE := true
 
 BOARD_LIBACRYL_DEFAULT_COMPOSITOR := fimg2d_gs101
 BOARD_LIBACRYL_G2D_HDR_PLUGIN := libacryl_hdr_plugin
+$(call soong_config_set,acryl,libacryl_g2d_hdr_plugin,//hardware/google/graphics/gs101/libacryl_plugins:libacryl_hdr_plugin)
 
 # HWCServices
 BOARD_USES_HWC_SERVICES := true
