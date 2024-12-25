35e03caa4b7e5c7a3aa0da0c58289a92244e34cd
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    gs101: overlay: Provide tethering configuration
    Matches PixelTetheringOverlay2021.
    
    Change-Id: I90ef9cd9c89b1bfacf5680cb1e42bb61ad275393

--- lineage_common.mk
+++ lineage_common.mk
@@ -34,5 +34,9 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
 PRODUCT_PACKAGES += \
     GoogleParts
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringOverlay
+
 # Touch
 include hardware/google/pixel/touch/device.mk
