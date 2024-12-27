2c0ef869dffcb8f6b19eb921f6fa31027b5e9df6
Spade Lee <spadelee@google.com>
2024-12-27 06:58:24 +0000

    pixelstats: gs101: remove firmware update path
    Bug: 386187651
    Test: local test pass
    Flag: EXEMPT refactor
    Change-Id: I2886e26186b12ffe34df6aa559bdf66e3e8f059d
    Signed-off-by: Spade Lee <spadelee@google.com>

--- pixelstats/service.cpp
+++ pixelstats/service.cpp
@@ -86,8 +86,7 @@ const struct SysfsCollector::SysfsPaths sysfs_paths = {
 
 const struct UeventListener::UeventPaths ueventPaths = {
         .AudioUevent = "/devices/virtual/amcs/amcs",
-        .TypeCPartnerUevent = "PRODUCT_TYPE=",
-        .FwUpdatePath = ""
+        .TypeCPartnerUevent = "PRODUCT_TYPE="
 };
 
 int main() {
