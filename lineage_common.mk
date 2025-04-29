4694eb0385b3943181352685fb3014e83c746242
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    gs101: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- lineage_common.mk
+++ lineage_common.mk
@@ -26,6 +26,10 @@ PRODUCT_PACKAGES += \
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 
+$(call soong_config_set,lineage_health,charging_control_supports_deadline,true)
+$(call soong_config_set,lineage_health,charging_control_supports_limit,true)
+$(call soong_config_set,lineage_health,charging_control_supports_toggle,false)
+
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     device/google/gs101/linker.config.json
