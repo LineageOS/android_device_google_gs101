95a7513af63b6ef6434f20c9b55543b420b7dad6
Michael Bestas <mkbestas@gmail.com>
2025-07-24 21:14:59 +0300

    gs101: Drop kernel copy
    We are using prebuilt boot.img. If we weren't using it, we would be
    using the vendor/lineage kernel.mk rules to copy the kernel.
    
    Change-Id: Ib8ab9416b96900f008dbce9d9f41f298a1b05f30

--- device.mk
+++ device.mk
@@ -67,8 +67,6 @@ PRODUCT_SOONG_NAMESPACES += \
 	device/google/gs101 \
 	device/google/gs101/powerstats
 
-LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image.lz4
-
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	ro.oem_unlock_supported=1
@@ -263,7 +261,6 @@ PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 
 # Init files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_KERNEL):kernel \
 	device/google/gs101/conf/init.gs101.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.gs101.usb.rc \
 	device/google/gs101/conf/ueventd.gs101.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
