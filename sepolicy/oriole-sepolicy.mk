a898a26c3d8c0196a8c7de44391124e93380ad7a
Michael Bestas <mkbestas@lineageos.org>
2025-12-01 23:48:59 +0200

    gs101: Move raviole sepolicy to device tree
    Change-Id: I24e6bc94b39ddb206a061c307e331b689900bd2a

--- sepolicy/oriole-sepolicy.mk
+++
@@ -1,5 +0,0 @@
-# Oriole only sepolicy
-BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/oriole
-
-BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
-BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l25
