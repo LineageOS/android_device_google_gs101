f1e4eca9d8645ccf8d28e1d8358773e4dbedcb12
Michael Bestas <mkbestas@lineageos.org>
2025-12-06 20:28:25 +0200

    gs101: Set missing properties from stock
    From BP3A
    
    Change-Id: Ic1fe53e051288f90b8d60679c8ee75685004dba8

--- device.mk
+++ device.mk
@@ -601,6 +601,10 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
 PRODUCT_PACKAGES += \
     GoogleParts
 
+# Properties
+TARGET_PRODUCT_PROP += device/google/gs101/product.prop
+TARGET_SYSTEM_PROP += device/google/gs101/system.prop
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringOverlay
