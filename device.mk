5a68d6ad3ea602a497d13c665ea2287d368760f8
Michael Bestas <mkbestas@lineageos.org>
2025-12-10 05:08:04 +0200

    properties: BP4A.251205.006
    Change-Id: Idc001a37251a1145dda8ecbd82ced65d8f2014a2

--- device.mk
+++ device.mk
@@ -599,7 +599,7 @@ PRODUCT_PACKAGES += \
 
 # Properties
 TARGET_PRODUCT_PROP += device/google/gs101/product.prop
-TARGET_SYSTEM_PROP += device/google/gs101/system.prop
+TARGET_SYSTEM_EXT_PROP += device/google/gs101/system_ext.prop
 
 # Tethering
 PRODUCT_PACKAGES += \
