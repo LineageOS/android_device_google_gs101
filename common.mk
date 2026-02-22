1608ad61c2e8a02784d729f09f1c9c8eedf52103
Michael Bestas <mkbestas@lineageos.org>
2026-02-23 00:02:48 +0200

    gs101: Fold device makefiles
    Change-Id: Ib1387b8c669be8d2f819d8ac8bb71e3fa67c502f

--- common.mk
+++ common.mk
@@ -291,6 +291,10 @@ include hardware/google/pixel/HardwareInfo/HardwareInfo.mk
 PRODUCT_COPY_FILES += \
     device/google/gs101/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
 
+# Android Verified Boot
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
     ANGLE
@@ -396,6 +400,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.dynamic_sensor_hal
 
+# Telephony
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.carrierlock.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.carrierlock.xml
+
 # Touch
 include hardware/google/pixel/touch/device.mk
 
@@ -406,3 +414,6 @@ DEVICE_MATRIX_FILE += \
     device/google/gs101/vintf/compatibility_matrix.xml
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
     device/google/gs101/vintf/device_framework_matrix_product.xml
+
+# ZRAM writeback
+include hardware/google/pixel/mm/device_gki.mk
