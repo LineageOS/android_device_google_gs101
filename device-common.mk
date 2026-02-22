32639022faf6a2fc253f9aae77913a7b71f0fa17
Michael Bestas <mkbestas@lineageos.org>
2026-02-22 23:21:33 +0200

    gs101: Move all properties to *.prop
    Change-Id: I894b24e68fd45341e69101f5e721c7927afece2a

--- device-common.mk
+++ device-common.mk
@@ -13,25 +13,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Set system properties identifying the chipset
-PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Google
-PRODUCT_VENDOR_PROPERTIES += ro.soc.model=Tensor
-
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.vendor.testing_battery_profile=2
-
-# The default value of this variable is false and should only be set to true when
-# the device allows users to retain eSIM profiles after factory reset of user data.
-PRODUCT_PRODUCT_PROPERTIES += \
-    masterclear.allow_retain_esim_profiles_after_fdr=true
-
 # ZramWriteback
 -include hardware/google/pixel/mm/device_gki.mk
-
-# Set thermal warm reset
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.thermal_warmreset = true
-
-# Trigger fsck on upgrade (305658663)
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.preventative_fsck = 1
