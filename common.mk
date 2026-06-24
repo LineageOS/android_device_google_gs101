9a92f098f5352c827074e89cd1925e1157eaf426
Michael Bestas <mkbestas@lineageos.org>
2026-06-24 17:54:03 +0300

    gs101: Remove dependency on gs-common
    We're switching to prebuilt dump_modem & bootctrl HAL
    
    Change-Id: Iaee3c64c22f41c7972d1ee2a282c7dad881e0cca

--- common.mk
+++ common.mk
@@ -11,9 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
-# Modem
-PRODUCT_PACKAGES += dump_modem
-
 # Thermal
 PRODUCT_PACKAGES += android.hardware.thermal-service.pixel
 
@@ -27,13 +24,6 @@ PRODUCT_PACKAGES += \
 # misc_writer
 PRODUCT_PACKAGES += \
     misc_writer
-
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot-service.default-pixel \
-    android.hardware.boot-service.default_recovery-pixel
-
-PRODUCT_SOONG_NAMESPACES += device/google/gs-common/bootctrl/aidl
 
 TARGET_BOARD_PLATFORM := gs101
 
